2df53458fd21686bb330a819ba03a1e525c6b284
Elkin Vasquez <73296467+ElkinV@users.noreply.github.com>
2025-05-04 14:28:26 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   integrations: [tailwind(), robotsTxt(), react()],
   output: "server",
-  site: "",
+  site: "https://elkin-vasquez.netlify.app",
   adapter: netlify()
 });
